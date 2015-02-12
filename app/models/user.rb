249575c06df00b525a09c00cde4b815e3f03ae60
elz <joel@gogreenride.com>
2015-02-11 23:46:59 -0500

    setup avatar for iphone

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
   has_many :tasks
 
   before_save :ensure_authentication_token
+  before_save :skip_confirmation!
   before_create :skip_confirmation!
 
   def skip_confirmation!
