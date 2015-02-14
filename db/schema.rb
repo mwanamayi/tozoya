7a549d0154dece04b6e1ff5fce6fbef004310ba3
elz <joel@gogreenride.com>
2015-02-14 00:09:44 -0500

    set avatar default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150212055327) do
+ActiveRecord::Schema.define(:version => 20150214050605) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -104,8 +104,8 @@ ActiveRecord::Schema.define(:version => 20150212055327) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",                                                                         :null => false
+    t.string   "encrypted_password",     :default => "",                                                                         :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -119,14 +119,14 @@ ActiveRecord::Schema.define(:version => 20150212055327) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.string   "authentication_token"
-    t.string   "username",               :default => "", :null => false
-    t.string   "first_name",             :default => "", :null => false
-    t.string   "last_name",              :default => "", :null => false
-    t.string   "status",                 :default => "", :null => false
-    t.string   "avatar"
+    t.string   "username",               :default => "",                                                                         :null => false
+    t.string   "first_name",             :default => "",                                                                         :null => false
+    t.string   "last_name",              :default => "",                                                                         :null => false
+    t.string   "status",                 :default => "",                                                                         :null => false
+    t.string   "avatar",                 :default => "http://thenittygritty.co/content/01-home/003-css-masking/mouse-black.png"
     t.integer  "school_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                                                                                     :null => false
+    t.datetime "updated_at",                                                                                                     :null => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
