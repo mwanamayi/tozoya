9b571ef4d4c8423ed218c828bce8bb518e0d8ad9
elz <joel@gogreenride.com>
2015-12-05 23:03:53 -0500

    landing page ready

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -7,10 +7,17 @@ $( document ).ready(function() {
     $(".landing_page .program").html('Ms. In International Business');
   }, 2500);
   setTimeout(function () {
+    $(".landing_page .avatar").children('img').attr("src", "/assets/jcy.jpg");
+  }, 2500);
+
+  setTimeout(function () {
     $(".landing_page .name").html('Elz Delhommeau');
   }, 4500);
   setTimeout(function () {
     $(".landing_page .program").html('Bachelors In Politics');
+  }, 4500);
+  setTimeout(function () {
+    $(".landing_page .avatar").children('img').attr("src", "/assets/avatar1.jpeg");
   }, 4500);
   setTimeout(function () {
     $(".landing_page .name").html("Bryan O'Kelley");
@@ -19,9 +26,15 @@ $( document ).ready(function() {
     $(".landing_page .program").html('Bachelors In Applied Physics');
   }, 6500);
   setTimeout(function () {
+    $(".landing_page .avatar").children('img').attr("src", "/assets/brian.jpeg");
+  }, 6500);
+  setTimeout(function () {
     $(".landing_page .name").html('Lisa Williams');
   }, 8500);
   setTimeout(function () {
     $(".landing_page .program").html('Student Exchange Program');
+  }, 8500);
+  setTimeout(function () {
+    $(".landing_page .avatar").children('img').attr("src", "/assets/lisa.jpeg");
   }, 8500);
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
     User.current = current_user
   end
 
-  def after_sign_in_path_for(resource_or_scope)
-    user_path(current_user.id)
-  end
+  # def after_sign_in_path_for(resource_or_scope)
+  #   user_path(current_user.id)
+  # end
 
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
+  # def after_sign_out_path_for(resource_or_scope)
+  #   root_path
+  # end
 
 end
