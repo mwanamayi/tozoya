b52a426c353d74bdfb4c36297b111d45df1e6d09
elz <joel@gogreenride.com>
2015-12-26 23:24:40 -0500

    redirect to home if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_current_user
+  before_filter :authenticate_user!
 
   def set_current_user
     User.current = current_user
+  end
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to root_path, :notice => "Oops! Looks like you haven't signed in yet!"
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
   end
 
 end
