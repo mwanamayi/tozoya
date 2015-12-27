63be7927a7b689576ab4256a9de9301e1e4f4d28
elz <joel@gogreenride.com>
2015-12-26 23:34:38 -0500

    redirect to home if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_current_user
-  before_filter :authenticate_user!
 
   def set_current_user
     User.current = current_user
