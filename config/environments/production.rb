ef177c7120781dee04961de9eb9540bd4e063562
elz <joel@gogreenride.com>
2015-12-30 18:55:30 -0500

    fix email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,18 +50,20 @@ Imin::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail
 
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['gmail_username'],
-      password:             ENV['gmail_password'],
-      authentication:       'plain',
-      enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #     address:              'smtp.gmail.com',
+  #     port:                 587,
+  #     domain:               'gmail.com',
+  #     user_name:            ENV['gmail_username'],
+  #     password:             ENV['gmail_password'],
+  #     authentication:       'plain',
+  #     enable_starttls_auto: true
+  # }
 
   # Enable threaded mode
   # config.threadsafe!
