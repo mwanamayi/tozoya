f244c83a0dc01b0fdbb3ecb89da2a055e5f9c330
elz <joel@gogreenride.com>
2016-01-10 20:59:12 -0500

    change landing page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,100 +1,167 @@
 /*
-	Alpha by HTML5 UP
+	Future Imperfect by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+$( document ).ready(function() {
+				console.log("loaded");
 
 (function($) {
 
 	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1280px)',
-		narrow: '(max-width: 980px)',
-		narrower: '(max-width: 840px)',
-		mobile: '(max-width: 736px)',
-		mobilep: '(max-width: 480px)'
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
 	});
 
 	$(function() {
 
 		var	$window = $(window),
 			$body = $('body'),
-			$header = $('#header'),
-			$banner = $('#banner');
+			$menu = $('#menu'),
+			$sidebar = $('#sidebar'),
+			$main = $('#main');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
-		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
 				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
 				);
 			});
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				alignment: 'right'
-			});
+		// IE<=9: Reverse order of main and sidebar.
+			if (skel.vars.IEVersion <= 9)
+				$main.insertAfter($sidebar);
 
-		// Off-Canvas Navigation.
+		// Menu.
 
-			// Navigation Button.
-				$(
-					'<div id="navButton">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+			$menu
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-menu-visible'
+				});
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+		// Search (header).
+			var $search = $('#search'),
+				$search_input = $search.find('input');
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navButton, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+			$body
+				.on('click', '[href="#search"]', function(event) {
 
-		// Header.
-		// If the header is using "alt" styling and #banner is present, use scrollwatch
-		// to revert it back to normal styling once the user scrolls past the banner.
-		// Note: This is disabled on mobile devices.
-			if (!skel.vars.mobile
-			&&	$header.hasClass('alt')
-			&&	$banner.length > 0) {
+					event.preventDefault();
 
-				$window.on('load', function() {
+					// Not visible?
+						if (!$search.hasClass('visible')) {
 
-					$banner.scrollwatch({
-						delay:		0,
-						range:		0.5,
-						anchor:		'top',
-						on:			function() { $header.addClass('alt reveal'); },
-						off:		function() { $header.removeClass('alt'); }
-					});
+							// Reset form.
+								$search[0].reset();
+
+							// Show.
+								$search.addClass('visible');
+
+							// Focus input.
+								$search_input.focus();
+
+						}
 
 				});
 
-			}
+			$search_input
+				.on('keydown', function(event) {
+
+					if (event.keyCode == 27)
+						$search_input.blur();
+
+				})
+				.on('blur', function() {
+					window.setTimeout(function() {
+						$search.removeClass('visible');
+					}, 100);
+				});
+
+		// Intro.
+			var $intro = $('#intro');
+
+			// Move to main on <=large, back to sidebar on >large.
+				skel
+					.on('+large', function() {
+						$intro.prependTo($main);
+					})
+					.on('-large', function() {
+						$intro.prependTo($sidebar);
+					});
 
 	});
 
-})(jQuery);
+});
+
+	$(".menu_button").click(function(){
+		$("#menu").fadeIn();
+	});
+
+	$("#close_menu").click(function(){
+		$("#menu").fadeOut();
+	});
+
+	$(document).mouseup(function(e) {
+
+  if( e.target.id != 'menu') {
+    $("#menu").fadeOut();
+  }
+
+	});
+
+	function resizeBootstrapMap() {
+    var mapParentWidth = $('.mapContainer').width();
+    $('.map').width(mapParentWidth /1.3);
+    $('.map').height(mapParentWidth / 2);
+    console.log(mapParentWidth);
+	}
+
+	$(window).resize(resizeBootstrapMap);
+	$(window).load(resizeBootstrapMap);
+
+    $("#users_search").keyup(function() {
+    	console.log("typing")
+      $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script");
+      return false;
+    });
+
+     $("#requests_search").submit(function() {
+    	console.log("typing")
+      $.get($("#requests_search").attr("action"), $("#requests_search").serialize(), null, "script");
+      return false;
+    });
+
+     $("#flights_search").submit(function() {
+    	console.log("typing")
+      $.get($("#flights_search").attr("action"), $("#flights_search").serialize(), null, "script");
+      return false;
+    });
+
+$(".scrollable").animate({ scrollTop: $(document).height()*2 }, "fast");
+  return false;
+
+  });
