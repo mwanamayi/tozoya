d66f7c104385c9e1d155a553f5bd0b95e791a1a3
elz <joel@gogreenride.com>
2016-01-12 19:34:36 -0500

    redirect invites to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_current_user
+  before_filter :redirect_all_invites
 
   def set_current_user
     User.current = current_user
@@ -15,6 +16,12 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, :notice => "Oops! Looks like you haven't signed in yet!"
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+
+  def redirect_all_invites
+    if request.original_url == ("http://localhost:3000/users/sign_up" || "https://localhost:3000/users/sign_up" || "http://apostell.com/users/sign_up" || "https://apostell.com/users/sign_up" || "http://apostell.com/users/sign_up" )
+      redirect_to root_path
     end
   end
 
