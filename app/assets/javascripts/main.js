4758335090c69baac1291b77d11b5f26a0d126fb
elz <joel@gogreenride.com>
2016-01-17 16:23:01 -0500

    revert to old invite system

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,3 @@
-/*
-	Future Imperfect by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 $( document ).ready(function() {
 
 	  $(".invite_friends").click(function() {
